dc2b9b5736094abf97c53ba216e0d7c150a9fc63
Matt Luedke <mluedke2@gmail.com>
2014-05-25 21:41:57 -0700

    add mars image

--- RunmasterGeneral/NewRunViewController.m
+++ RunmasterGeneral/NewRunViewController.m
@@ -127,9 +127,6 @@ static NSString * const detailSegueName = @"ShowDetails";
     
     if (abs(howRecent) < 10.0 && newLocation.horizontalAccuracy < 50) {
         
-        NSLog(@"newLocation.coordinate.latitude: %f", newLocation.coordinate.latitude);
-        NSLog(@"newLocation.coordinate.longitude: %f", newLocation.coordinate.longitude);
-        
         // update distance
         if (self.locations.count > 0) {
             self.distance += [newLocation distanceFromLocation:self.locations.lastObject];
