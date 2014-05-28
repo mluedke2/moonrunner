9dffa516faf048fa688a73b2dd5fd8df3f8de0f5
Matt Luedke <mluedke2@gmail.com>
2014-05-28 02:26:14 -0700

    code cleanup

--- MoonRunner/Controller/AppDelegate.m
+++ MoonRunner/Controller/AppDelegate.m
@@ -14,18 +14,6 @@
     homeViewController.managedObjectContext = self.managedObjectContext;
     return YES;
 }
-							
-- (void)applicationWillResignActive:(UIApplication *)application
-{}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{}
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
