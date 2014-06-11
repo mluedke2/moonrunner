cbc1e6e8bba2f9f0af59eaab80b720002251b2eb
Matt Luedke <mluedke2@gmail.com>
2014-06-11 16:46:23 -0700

    cleanup annotation header

--- MoonRunner/Model/BadgeAnnotation.h
+++ MoonRunner/Model/BadgeAnnotation.h
@@ -6,14 +6,10 @@
 //  Copyright (c) 2014 Matt Luedke. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
 #import <MapKit/MapKit.h>
 
 @interface BadgeAnnotation : MKPointAnnotation
 
-//@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
-//@property (nonatomic, readonly, copy) NSString *title;
-//@property (nonatomic, readonly, copy) NSString *subtitle;
 @property (strong, nonatomic) NSString *imageName;
 
 @end
